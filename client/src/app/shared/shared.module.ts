e9b44ed3dc6b4616cb74417ddc8fcb0cc17c181f
vivek-baliyan <vivekbr001@gmail.com>
2024-03-22 15:54:04 +0530

    ssl certificate added

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MatSidenavModule],
+  exports: [MatSidenavModule],
+})
+export class SharedModule {}
