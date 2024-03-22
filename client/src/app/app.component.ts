d3ddb9abbdc7a7d71df45f5d57730ac5589cf3f0
vivek-baliyan <vivekbr001@gmail.com>
2024-03-22 14:54:46 +0530

    created new client project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,41 +1,13 @@
-import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { User } from './_models/user';
-import { UsersService } from './_services/users.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  opened: boolean = true;
-  model: any = {};
-
-  constructor(public usersService: UsersService, private router: Router) {}
-
-  ngOnInit(): void {
-    this.setCurrentUser();
-  }
-
-  login() {
-    this.usersService.login(this.model).subscribe((response) => {
-      this.router.navigateByUrl('/budget');
-    });
-  }
-
-  logout() {
-    this.usersService.logout();
-    this.router.navigateByUrl('/');
-  }
-
-  setCurrentUser() {
-    const user: User = JSON.parse(localStorage.getItem('user'));
-    this.usersService.setCurrentUser(user);
-  }
-
-  toggleSidebar() {
-    this.opened = !this.opened;
-  }
+export class AppComponent {
+  title = 'client';
 }
